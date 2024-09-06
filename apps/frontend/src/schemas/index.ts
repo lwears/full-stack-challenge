1c539e05a07f129eb46a8c30f68eafa35d47093c
Liam Wears <liamwears@fastmail.com>
2024-09-06 19:54:22 +0200

    refactor: remove profileSchema from FE

--- apps/frontend/src/schemas/index.ts
+++ apps/frontend/src/schemas/index.ts
@@ -1,23 +1,8 @@
 import { z } from 'zod'
 import { zPhoneNumber } from '../utils'
 
-export const profileSchema = z.object({
-  name: z
-    .string({
-      required_error: 'name is required',
-      invalid_type_error: 'name must be a string',
-    })
-    .toLowerCase()
-    .trim()
-    .min(1, { message: 'name must be minimum 1 character' })
-    .max(45)
-    .refine((value) => Number.isNaN(Number(value)), {
-      message: 'value should not be a number',
-    }),
-  email: z.string().email(),
-})
-
 export const phoneSchema = z.object({
+  countryCode: z.string(),
   phoneNumber: zPhoneNumber,
 })
 
@@ -29,6 +14,5 @@ export const codeSchema = z.object({
     .regex(/^\d+$/, 'Must contain only digits'),
 })
 
-export type ProfileFormData = z.infer<typeof profileSchema>
 export type PhoneFormData = z.infer<typeof phoneSchema>
 export type CodeFormData = z.infer<typeof codeSchema>
