f9c6b18cacfcf73a89cf7400b6899d77a3af1e82
Liam Wears <liamwears@fastmail.com>
2024-09-19 11:43:27 +0200

    ci: trying to fix functions deployment

--- apps/functions/esbuild.config.cjs
+++ apps/functions/esbuild.config.cjs
@@ -11,7 +11,7 @@ require('esbuild')
     // Optional and for development only. This provides the ability to
     // map the built code back to the original source format when debugging.
     //sourcemap: 'inline',
-    external: ['../../node_modules/*'],
+    external: ['./node_modules/*'],
     logLevel: 'info',
     tsconfig: './tsconfig.json',
   })
